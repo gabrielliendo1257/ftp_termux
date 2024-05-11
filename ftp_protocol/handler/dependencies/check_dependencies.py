bf1949df0580cac2b3d0c5e2623e0de7c9cb1d68
Piter N. <gabrielliendo1257>
2024-05-11 17:46:34 -0500

    New directories added to .gitignore

--- ftp_protocol/handler/dependencies/check_dependencies.py
+++ ftp_protocol/handler/dependencies/check_dependencies.py
@@ -8,6 +8,6 @@ class CheckDependencies:
     def exist_executables(dependencies):
         for dependency, exec in dependencies.items():
             subprocess.run(exec, text=True, shell=True)
-            out = subprocess.run("powershell.exe $?", text=True, shell=True)
+            out = subprocess.run("$?", text=True, shell=True)
             if not bool(out):
                 print(f"La dependencia ´{dependency}´ no se reconoce en su sistema.")
