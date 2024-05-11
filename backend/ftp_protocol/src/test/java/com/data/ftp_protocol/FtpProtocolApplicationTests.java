2b63d2005f3d25d7a99a2d9658907483b4753d9a
Piter N <gabrielliendo1257@gmail.com>
2024-05-11 10:57:09 -0500

    Adding the Backend Folder

---
+++ backend/ftp_protocol/src/test/java/com/data/ftp_protocol/FtpProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.ftp_protocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtpProtocolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
